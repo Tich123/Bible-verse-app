a44d02fe8910c5251678fac44a7840dd52fc0227
Tich123 <mashavatich@gmail.com>
2024-07-15 16:51:16 -0700

    my app

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
  
 const express = require('express');  
 const app = express();  
-const csv = require('csv-parser');  
+const csv = require('csv-parse');  
 const fs = require('fs');  
   
 app.use(express.static('public'));  
