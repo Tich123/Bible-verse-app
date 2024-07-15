da66bb68615913b3171a5b45f01fdcc68b5d1483
Tich123 <mashavatich@gmail.com>
2024-07-15 16:33:37 -0700

    my app

--- server/app.js
+++ server/app.js
@@ -7,29 +7,32 @@ const fs = require('fs');
 app.use(express.static('public'));  
   
 let bibleData = [];  
-  
-fs.readFile('kjv_strongs.csv', (err, data) => {  
-  if (err) {  
-   console.error(err);  
-   return;  
-  }  
-  csv.parse(data, (err, data) => {  
-   if (err) {  
-    console.error(err);  
-    return;  
-   }  
-   console.log('Parsed data:', data);  
-   bibleData = data.map((row) => {  
-    return {  
-      Book: row['Book Name'],  
-      Chapter: parseInt(row.Chapter),  
-      Verse: parseInt(row.Verse),  
-      Text: row.Text,  
-    };  
-   });  
-   console.log('Mapped data:', bibleData);  
-  });  
-});  
+
+
+console.log('Reading file...');
+fs.readFile('kjv_strongs.csv', (err, data) => {
+  if (err) {
+   console.error(err);
+   return;
+  }
+  console.log('File read successfully!');
+  csv.parse(data, (err, data) => {
+   if (err) {
+    console.error(err);
+    return;
+   }
+   console.log('Parsed data:', data);
+   bibleData = data.map((row) => {
+    return {
+      Book: row['Book Name'],
+      Chapter: parseInt(row.Chapter),
+      Verse: parseInt(row.Verse),
+      Text: row.Text,
+    };
+   });
+   console.log('Mapped data:', bibleData);
+  });
+});
   
 app.get('/search', (req, res) => {  
   //...  
