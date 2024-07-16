80384d4d14d531455eebfdb26ac2924bccfcf7d4
Tich123 <mashavatich@gmail.com>
2024-07-15 17:27:03 -0700

    my app

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ app.get('/search', (req, res) => {
   //...  
 });  
   
-app.listen(3000, () => {  
-  console.log('Server started on port 3000');  
+app.listen(3001, () => {  
+  console.log('Server started on port 3001');  
 });  
 
